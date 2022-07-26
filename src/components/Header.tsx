b68bfa37756b4223833b5c9e7f4a2e9fd0287e80
Aaron Au <aaron.au@me.com>
2022-07-26 01:17:46 -0600

    Fix typo in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import "./Header.css"
 const Header = () => {
   return (
     <header>
-      A Simple Todo List App Using React & Tyescript
+      A Simple Todo List App Using React & Typescript
     </header>
   );
 };
